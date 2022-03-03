4382455bbd2de0760908b14bd157127c03fbbd05
Mila B <milabochkova03@gmail.com>
2022-03-03 07:09:56 +0300

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by Mila B on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
